b517ddd86394d943ae1337b0be26b29d2b946b48
Ragibn5 <ragibn5@gmail.com>
2023-06-30 22:56:42 +0600

    Made StateBase parcelable, and renamed StateBas.requestCode to StateBas.code

--- PiTrackerCommons/src/main/java/com/vivasoft/pitrackercommons/models/StateBase.java
+++ PiTrackerCommons/src/main/java/com/vivasoft/pitrackercommons/models/StateBase.java
@@ -1,27 +1,60 @@
 package com.vivasoft.pitrackercommons.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import java.io.Serializable;
-
-public class StateBase implements Serializable {
+public class StateBase implements Parcelable {
   @NonNull
   private final String packageName;
-  private final int requestCode;
+  private final int code;
   private final boolean success;
   @Nullable
   private final String data;
 
   public StateBase(
     @NonNull String packageName,
-    int requestCode,
+    int code,
     boolean success,
     @Nullable String data) {
     this.packageName = packageName;
-    this.requestCode = requestCode;
+    this.code = code;
     this.success = success;
     this.data = data;
+  }
+
+  protected StateBase(Parcel in) {
+    packageName = in.readString();
+    code = in.readInt();
+    success = in.readByte() != 0;
+    data = in.readString();
+  }
+
+  public static final Creator<StateBase> CREATOR = new Creator<StateBase>() {
+    @Override
+    public StateBase createFromParcel(Parcel in) {
+      return new StateBase(in);
+    }
+
+    @Override
+    public StateBase[] newArray(int size) {
+      return new StateBase[size];
+    }
+  };
+
+  @Override
+  public int describeContents() {
+    return 0;
+  }
+
+  @Override
+  public void writeToParcel(@NonNull Parcel dest, int flags) {
+    dest.writeString(packageName);
+    dest.writeInt(code);
+    dest.writeByte((byte) (success ? 1 : 0));
+    dest.writeString(data);
   }
 
   @NonNull
@@ -29,8 +62,8 @@ public class StateBase implements Serializable {
     return packageName;
   }
 
-  public int getRequestCode() {
-    return requestCode;
+  public int getCode() {
+    return code;
   }
 
   public boolean isSuccess() {
