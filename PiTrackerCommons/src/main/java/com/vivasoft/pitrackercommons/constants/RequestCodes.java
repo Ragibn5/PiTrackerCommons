50be9e70a3e1936051fc563494c3c6ce0d507243
Ragibn5 <ragibn5@gmail.com>
2023-06-30 20:16:57 +0600

    Added data field (String) in StateBase.java & a new response code (ResponseCodes.MALFORMED_REQUEST = 299)

--- PiTrackerCommons/src/main/java/com/vivasoft/pitrackercommons/constants/RequestCodes.java
+++ PiTrackerCommons/src/main/java/com/vivasoft/pitrackercommons/constants/RequestCodes.java
@@ -1,23 +1,6 @@
 package com.vivasoft.pitrackercommons.constants;
 
-import com.vivasoft.pitrackercommons.models.PackageConfig;
-
 public final class RequestCodes {
-  /**
-   * Requires the message object to have a bundle with the following:
-   * <b>packageName</b><br>
-   * Package name of the requesting package.
-   */
   public static final int RETURN_MY_CONFIG = 101;
-
-
-  /**
-   * Requires the message object to have a bundle with the following:
-   * <b>packageName</b><br>
-   * Package name of the requesting package.
-   * <br><br>
-   * <b>packageConfig</b><br>
-   * Configuration of type {@link PackageConfig}
-   */
   public static final int CREATE_MY_CONFIG = 102;
 }
--- PiTrackerCommons/src/main/java/com/vivasoft/pitrackercommons/constants/ResponseCodes.java
+++ PiTrackerCommons/src/main/java/com/vivasoft/pitrackercommons/constants/ResponseCodes.java
@@ -1,7 +1,9 @@
 package com.vivasoft.pitrackercommons.constants;
 
 public final class ResponseCodes {
-
-  public static final int MALFORMED_REQUEST = 200;
+  // return codes
   public static final int RETURNING_YOUR_CONFIG = 201;
+
+  // error codes
+  public static final int MALFORMED_REQUEST = 299;
 }
