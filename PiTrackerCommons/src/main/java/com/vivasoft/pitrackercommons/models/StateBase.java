957f2b19138d8685373fe14a427821888c1c102d
Ragibn5 <ragibn5@gmail.com>
2023-06-30 19:29:41 +0600

    Added package name in StateBase.java

--- PiTrackerCommons/src/main/java/com/vivasoft/pitrackercommons/models/StateBase.java
+++ PiTrackerCommons/src/main/java/com/vivasoft/pitrackercommons/models/StateBase.java
@@ -1,21 +1,31 @@
 package com.vivasoft.pitrackercommons.models;
 
+import androidx.annotation.NonNull;
+
 import java.io.Serializable;
 
 public class StateBase implements Serializable {
-  private final boolean success;
+  @NonNull
+  private final String packageName;
   private final int requestCode;
+  private final boolean success;
 
-  public StateBase(boolean success, int requestCode) {
-    this.success = success;
+  public StateBase(@NonNull String packageName, int requestCode, boolean success) {
+    this.packageName = packageName;
     this.requestCode = requestCode;
+    this.success = success;
   }
 
-  public boolean isSuccess() {
-    return success;
+  @NonNull
+  public String getPackageName() {
+    return packageName;
   }
 
   public int getRequestCode() {
     return requestCode;
+  }
+
+  public boolean isSuccess() {
+    return success;
   }
 }
