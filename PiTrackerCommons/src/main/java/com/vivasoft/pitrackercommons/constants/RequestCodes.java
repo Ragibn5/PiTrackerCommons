45efc5d7cd3249a0fe48dbc0960dea5f5e96650e
Ragibn5 <ragibn5@gmail.com>
2023-07-01 12:04:18 +0600

    Added new response codes and extended response code range

--- PiTrackerCommons/src/main/java/com/vivasoft/pitrackercommons/constants/RequestCodes.java
+++ PiTrackerCommons/src/main/java/com/vivasoft/pitrackercommons/constants/RequestCodes.java
@@ -1,6 +1,11 @@
 package com.vivasoft.pitrackercommons.constants;
 
+/**
+ * Request codes: [1 -> 999]
+ */
 public final class RequestCodes {
-  public static final int RETURN_MY_CONFIG = 101;
-  public static final int CREATE_MY_CONFIG = 102;
+  public static final int PING = 1;
+
+  public static final int RETURN_MY_CONFIG = 2;
+  public static final int CREATE_MY_CONFIG = 3;
 }
--- PiTrackerCommons/src/main/java/com/vivasoft/pitrackercommons/constants/ResponseCodes.java
+++ PiTrackerCommons/src/main/java/com/vivasoft/pitrackercommons/constants/ResponseCodes.java
@@ -1,12 +1,12 @@
 package com.vivasoft.pitrackercommons.constants;
 
+/**
+ * Response codes: [1000 -> 1999]
+ */
 public final class ResponseCodes {
-  // success codes
-  public static final int SUCCESS = 200;
+  public static final int SENDING_TEXT_NOTIFICATION = 1000;
+  public static final int RETURNING_YOUR_CONFIG = 1001;
+  public static final int CREATED_YOUR_CONFIG = 1002;
 
-  // return codes
-  public static final int RETURNING_YOUR_CONFIG = 201;
-
-  // error codes
-  public static final int MALFORMED_REQUEST = 299;
+  public static final int MALFORMED_REQUEST = 1999;
 }
