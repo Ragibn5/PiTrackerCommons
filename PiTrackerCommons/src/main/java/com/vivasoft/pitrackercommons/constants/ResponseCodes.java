47ccea628924e6f5a47d64fb4fcd2131eb0212a7
Ragibn5 <ragibn5@gmail.com>
2023-07-01 10:22:37 +0600

    Added new response code (Success = 200)

--- PiTrackerCommons/src/main/java/com/vivasoft/pitrackercommons/constants/ResponseCodes.java
+++ PiTrackerCommons/src/main/java/com/vivasoft/pitrackercommons/constants/ResponseCodes.java
@@ -1,6 +1,9 @@
 package com.vivasoft.pitrackercommons.constants;
 
 public final class ResponseCodes {
+  // success codes
+  public static final int SUCCESS = 200;
+
   // return codes
   public static final int RETURNING_YOUR_CONFIG = 201;
 
