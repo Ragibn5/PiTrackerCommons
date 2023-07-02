37903ab5cb98aa699153e332b6e6d187e20691ad
Ragibn5 <ragibn5@gmail.com>
2023-07-02 13:56:18 +0600

    Added IncomingMessageHandler.java & related components.

---
+++ PiTrackerCommons/src/main/java/com/vivasoft/pitrackercommons/handlers/IncomingMessageHandler.java
@@ -0,0 +1,29 @@
+package com.vivasoft.pitrackercommons.handlers;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+
+import androidx.annotation.NonNull;
+
+public class IncomingMessageHandler extends Handler {
+  @NonNull
+  private final MessageCallback messageCallback;
+
+  public IncomingMessageHandler(
+    @NonNull Looper looper,
+    @NonNull MessageCallback messageCallback) {
+    super(looper);
+    this.messageCallback = messageCallback;
+  }
+
+  @Override
+  public void handleMessage(@NonNull Message msg) {
+    super.handleMessage(msg);
+    messageCallback.onMessage(msg);
+  }
+
+  public static interface MessageCallback {
+    public void onMessage(Message message);
+  }
+}
