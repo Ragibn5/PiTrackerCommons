50cdce7f74ee12ed5e9f28f543bd00ac72723333
Ragibn5 <ragibn5@gmail.com>
2023-08-10 12:23:29 +0600

    Added a new response code.

--- PiTrackerCommons/src/main/java/com/vivasoft/pitrackercommons/constants/ResponseCodes.java
+++ PiTrackerCommons/src/main/java/com/vivasoft/pitrackercommons/constants/ResponseCodes.java
@@ -7,6 +7,7 @@ public final class ResponseCodes {
   public static final int SENDING_TEXT_NOTIFICATION = 1000;
   public static final int RETURNING_YOUR_CONFIG = 1001;
   public static final int CREATED_YOUR_CONFIG = 1002;
+  public static final int OPENED_PERMISSION_MANAGER_ACTIVITY = 1003;
 
   public static final int PERMISSIONS_NOT_GRANTED = 1997;
   public static final int UNKNOWN_REQUEST = 1998;
