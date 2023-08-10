d2bdc29b71a8900d0eb5f46a96d7fd7d838aecd8
Ragibn5 <ragibn5@gmail.com>
2023-08-10 10:58:18 +0600

    Added new response and request codes.

--- PiTrackerCommons/src/main/java/com/vivasoft/pitrackercommons/constants/RequestCodes.java
+++ PiTrackerCommons/src/main/java/com/vivasoft/pitrackercommons/constants/RequestCodes.java
@@ -8,4 +8,5 @@ public final class RequestCodes {
 
   public static final int RETURN_MY_CONFIG = 2;
   public static final int CREATE_MY_CONFIG = 3;
+  public static final int OPEN_PERMISSION_MANAGER_ACTIVITY = 4;
 }
--- PiTrackerCommons/src/main/java/com/vivasoft/pitrackercommons/constants/ResponseCodes.java
+++ PiTrackerCommons/src/main/java/com/vivasoft/pitrackercommons/constants/ResponseCodes.java
@@ -8,6 +8,7 @@ public final class ResponseCodes {
   public static final int RETURNING_YOUR_CONFIG = 1001;
   public static final int CREATED_YOUR_CONFIG = 1002;
 
+  public static final int PERMISSIONS_NOT_GRANTED = 1997;
   public static final int UNKNOWN_REQUEST = 1998;
   public static final int MALFORMED_REQUEST = 1999;
 }
