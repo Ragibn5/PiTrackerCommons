0c6ba9f3e95581e76e46d077138f6aca08a04b3b
Ragibn5 <ragibn5@gmail.com>
2023-08-18 23:10:23 +0600

    Added config wrapper

--- PiTrackerCommons/src/main/java/com/vivasoft/pitrackercommons/models/ConfigCollection.java
+++ PiTrackerCommons/src/main/java/com/vivasoft/pitrackercommons/models/ConfigCollection.java
@@ -5,8 +5,10 @@ import androidx.annotation.NonNull;
 import java.io.Serializable;
 
 public class ConfigCollection implements Serializable {
-  @NonNull LogPostConfig logPostConfig;
-  @NonNull LocationPostConfig locationPostConfig;
+  @NonNull
+  LogPostConfig logPostConfig;
+  @NonNull
+  LocationPostConfig locationPostConfig;
 
   public ConfigCollection(@NonNull LogPostConfig logPostConfig, @NonNull LocationPostConfig locationPostConfig) {
     this.logPostConfig = logPostConfig;
