1abf3c0969f776d81c69d64e8a8f80d05dc63a95
nowrose.ragib <nowrose.ragib@vivacomsolutions.com>
2023-09-13 19:42:20 +0600

    Modified Param model to accept type enum

--- PiTrackerCommons/src/main/java/com/vivasoft/pitrackercommons/models/Param.java
+++ PiTrackerCommons/src/main/java/com/vivasoft/pitrackercommons/models/Param.java
@@ -9,10 +9,13 @@ public class Param implements Serializable {
   private final String key;
   @NonNull
   private final Object value;
+  @NonNull
+  private final ParamType paramType;
 
-  public Param(@NonNull String key, @NonNull Object value) {
+  private Param(@NonNull String key, @NonNull Object value, @NonNull ParamType paramType) {
     this.key = key;
     this.value = value;
+    this.paramType = paramType;
   }
 
   @NonNull
@@ -23,5 +26,42 @@ public class Param implements Serializable {
   @NonNull
   public Object getValue() {
     return value;
+  }
+
+  @NonNull
+  public ParamType getParamType() {
+    return paramType;
+  }
+
+  public Param getTypeFixedInstance() {
+    switch (paramType) {
+      case TYPE_BOOL:
+        return new Param(key, ((boolean) value), paramType);
+      case TYPE_INT_32:
+        return new Param(key, ((int) value), paramType);
+      case TYPE_INT_64:
+        return new Param(key, ((long) value), paramType);
+      case TYPE_DOUBLE:
+        return new Param(key, ((double) value), paramType);
+      default:
+        return new Param(key, value.toString(), paramType);
+    }
+  }
+
+  public enum ParamType {
+    TYPE_BOOL(1),
+    TYPE_INT_32(2),
+    TYPE_INT_64(3),
+    TYPE_DOUBLE(4),
+    TYPE_STRING(5);
+    private final int value;
+
+    ParamType(int value) {
+      this.value = value;
+    }
+
+    public int getValue() {
+      return value;
+    }
   }
 }
