51eeeb46c7b5a695a00d02c88c7a7bb87721ebcf
nowrose.ragib <nowrose.ragib@vivacomsolutions.com>
2023-09-22 17:44:40 +0600

    Modified PackageConfig to take package name.

--- PiTrackerCommons/src/main/java/com/vivasoft/pitrackercommons/models/PackageConfig.java
+++ PiTrackerCommons/src/main/java/com/vivasoft/pitrackercommons/models/PackageConfig.java
@@ -6,13 +6,21 @@ import java.io.Serializable;
 
 public class PackageConfig implements Serializable {
   @NonNull
+  final String packageName;
+  @NonNull
   private final LogPostConfig logPostConfig;
   @NonNull
   private final LocationPostConfig locationPostConfig;
 
-  public PackageConfig(@NonNull LogPostConfig logPostConfig, @NonNull LocationPostConfig locationPostConfig) {
+  public PackageConfig(@NonNull String packageName, @NonNull LogPostConfig logPostConfig, @NonNull LocationPostConfig locationPostConfig) {
+    this.packageName = packageName;
     this.logPostConfig = logPostConfig;
     this.locationPostConfig = locationPostConfig;
+  }
+
+  @NonNull
+  public String getPackageName() {
+    return packageName;
   }
 
   @NonNull
