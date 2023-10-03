e5109aba36097d0c84a637d83d9605c988957c2a
nowrose.ragib <nowrose.ragib@vivacomsolutions.com>
2023-10-03 21:17:47 +0600

    Added new response code.

--- PiTrackerCommons/src/main/java/com/vivasoft/pitrackercommons/constants/ResponseCodes.java
+++ PiTrackerCommons/src/main/java/com/vivasoft/pitrackercommons/constants/ResponseCodes.java
@@ -8,6 +8,7 @@ public final class ResponseCodes {
   public static final int RETURNING_YOUR_CONFIG = 1001;
   public static final int CREATED_YOUR_CONFIG = 1002;
   public static final int OPENED_PERMISSION_MANAGER_ACTIVITY = 1003;
+  public static final int OS_DISALLOWED_BACKGROUND_ACTIVITY_STARTUP = 1004;
 
   public static final int PERMISSIONS_NOT_GRANTED = 1997;
   public static final int UNKNOWN_REQUEST = 1998;
